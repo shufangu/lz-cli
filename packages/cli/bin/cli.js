a10f35361d9e6235b8213ba0b0807e70a2abe968
乱粥 <1259857279@qq.com>
2024-03-19 22:17:04 +0800

    feat: 初始化

---
+++ packages/cli/bin/cli.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import entry from "../lib/index.js";
+
+entry(process.argv.slice(2));
---
+++ packages/cli/lib/index.js
@@ -0,0 +1,3 @@
+export default function (args) {
+  console.log("this is entry", args);
+}
